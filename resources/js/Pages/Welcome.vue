20a1a5edb2a9a9fb2349f7cba9dc4384a5644c76
Zeno Frooninckx <zeno.frooninckx@hotmail.com>
2021-02-17 20:47:34 +0100

    inertiajs + vuejs installation

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>Dit is een test {{ foo }}</div>
+</template>
+
+<script>
+    export default {
+        props: {
+            foo: String
+        }
+    }
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
 require('./bootstrap');
+
+/*
+|----------------------------------------------------------------
+| Vue 3
+|----------------------------------------------------------------
+*/
+
+import { createApp, h } from 'vue'
+import { App, plugin } from '@inertiajs/inertia-vue3'
+
+const el = document.getElementById('app')
+
+createApp({
+    render: () => h(App, {
+        initialPage: JSON.parse(el.dataset.page),
+        resolveComponent: name => require(`./Pages/${name}`).default,
+    })
+}).use(plugin).mount(el)
+
+// progressbar
+import { InertiaProgress } from '@inertiajs/progress'
+
+InertiaProgress.init()
