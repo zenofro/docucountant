cd19fc73d3a0b8234e6022cccb6ce524bcf06579
Zeno Frooninckx <zeno.frooninckx@hotmail.com>
2021-02-17 23:59:47 +0100

    installed ziggy

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,12 +4,13 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
-    <title>{{ config('app.name') }} - {{ $title }}</title>
+    <title>{{ config('app.name') }} {{ isset($title) ? ' - ' . $title : null }}</title>
 
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
 </head>
 <body>
+    @routes
     @inertia
 </body>
 </html>
