3f17291055ef6fff7e56e6f38aab9cd26b0939e6
Zeno Frooninckx <zeno.frooninckx@hotmail.com>
2021-02-19 00:48:05 +0100

    added columns

--- resources/js/Pages/Admin/Users/Create.vue
+++ resources/js/Pages/Admin/Users/Create.vue
@@ -1,35 +1,49 @@
 <template>
     <div class="container box">
         <form @submit.prevent="form.post(route('admin.users.store'))">
-            <!-- name -->
-            <b-field :message="form.errors.name"
-                     :type="form.errors.name ? 'is-danger' : null"
-                     label="Name">
-                <b-input v-model="form.name" type="text"></b-input>
-            </b-field>
+            <div class="columns">
+                <div class="column">
+                    <!-- name -->
+                    <b-field :message="form.errors.name"
+                             :type="form.errors.name ? 'is-danger' : null"
+                             label="Name">
+                        <b-input v-model="form.name" type="text"></b-input>
+                    </b-field>
+                </div>
 
-            <!-- email -->
-            <b-field :message="form.errors.email"
-                     :type="form.errors.email ? 'is-danger' : null"
-                     label="Email">
-                <b-input v-model="form.email" type="email"></b-input>
-            </b-field>
+                <div class="column">
+                    <!-- email -->
+                    <b-field :message="form.errors.email"
+                             :type="form.errors.email ? 'is-danger' : null"
+                             label="Email">
+                        <b-input v-model="form.email" type="email"></b-input>
+                    </b-field>
+                </div>
+            </div>
 
-            <!-- password -->
-            <b-field :message="form.errors.password"
-                     :type="form.errors.password ? 'is-danger' : null"
-                     label="Password">
-                <b-input v-model="form.password" password-reveal type="password"></b-input>
-            </b-field>
+            <div class="columns">
+                <div class="column">
+                    <!-- password -->
+                    <b-field :message="form.errors.password"
+                             :type="form.errors.password ? 'is-danger' : null"
+                             label="Password">
+                        <b-input v-model="form.password" password-reveal type="password"></b-input>
+                    </b-field>
+                </div>
 
-            <!-- password confirmation -->
-            <b-field label="Confirm password">
-                <b-input v-model="form.password_confirmation" password-reveal type="password"></b-input>
-            </b-field>
+                <div class="column">
+                    <!-- password confirmation -->
+                    <b-field label="Confirm password">
+                        <b-input v-model="form.password_confirmation" password-reveal type="password"></b-input>
+                    </b-field>
+                </div>
+            </div>
 
             <!-- submit -->
             <div class="is-flex is-justify-content-flex-end pt-3">
-                <b-button :disabled="form.processing" native-type="submit" type="is-success" :loading="form.processing">Create</b-button>
+                <b-button :disabled="form.processing" :loading="form.processing" native-type="submit" type="is-success">
+                    Create
+                </b-button>
             </div>
         </form>
     </div>
--- resources/js/Pages/Admin/Users/Edit.vue
+++ resources/js/Pages/Admin/Users/Edit.vue
@@ -1,19 +1,25 @@
 <template>
     <div class="container box">
         <form @submit.prevent="form.patch(route('admin.users.update', user.id))">
-            <!-- name -->
-            <b-field :message="form.errors.name"
-                     :type="form.errors.name ? 'is-danger' : null"
-                     label="Name">
-                <b-input v-model="form.name" type="text"></b-input>
-            </b-field>
+            <div class="columns">
+                <div class="column">
+                    <!-- name -->
+                    <b-field :message="form.errors.name"
+                             :type="form.errors.name ? 'is-danger' : null"
+                             label="Name">
+                        <b-input v-model="form.name" type="text"></b-input>
+                    </b-field>
+                </div>
 
-            <!-- email -->
-            <b-field :message="form.errors.email"
-                     :type="form.errors.email ? 'is-danger' : null"
-                     label="Email">
-                <b-input v-model="form.email" type="email"></b-input>
-            </b-field>
+                <div class="column">
+                    <!-- email -->
+                    <b-field :message="form.errors.email"
+                             :type="form.errors.email ? 'is-danger' : null"
+                             label="Email">
+                        <b-input v-model="form.email" type="email"></b-input>
+                    </b-field>
+                </div>
+            </div>
 
             <!-- submit -->
             <div class="is-flex is-justify-content-flex-end pt-3">
