caa70e7b34fdc06296a6256b986daf7f7ea061f2
Zeno Frooninckx <zeno.frooninckx@hotmail.com>
2021-02-18 23:25:36 +0100

    create user

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -5,21 +5,27 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Inertia\Inertia;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Admin/Users/Index', [
-            'users' => User::all()->map(function ($user) {
-                return [
-                    'id' => $user->id,
-                    'name' => $user->name,
-                    'email' => $user->email,
-                ];
-            }),
-        ]);
+        return Inertia::render(
+            'Admin/Users/Index',
+            [
+                'users' => User::all()->map(
+                    function ($user) {
+                        return [
+                            'id' => $user->id,
+                            'name' => $user->name,
+                            'email' => $user->email,
+                        ];
+                    }
+                ),
+            ]
+        );
     }
 
     public function create()
@@ -29,7 +35,20 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'confirmed'],
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return redirect()->route('admin.users.index')
+            ->with('success', 'User created successfully');
     }
 
     public function show($id)
--- resources/js/Pages/Admin/Users/Create.vue
+++ resources/js/Pages/Admin/Users/Create.vue
@@ -1,11 +1,52 @@
 <template>
-    <div>
-        Create
+    <div class="container box">
+        <form @submit.prevent="form.post(route('admin.users.store'))">
+            <!-- name -->
+            <b-field :message="form.errors.name"
+                     :type="form.errors.name ? 'is-danger' : null"
+                     label="Name">
+                <b-input v-model="form.name" type="text"></b-input>
+            </b-field>
+
+            <!-- email -->
+            <b-field :message="form.errors.email"
+                     :type="form.errors.email ? 'is-danger' : null"
+                     label="Email">
+                <b-input v-model="form.email" type="email"></b-input>
+            </b-field>
+
+            <!-- password -->
+            <b-field :message="form.errors.password"
+                     :type="form.errors.password ? 'is-danger' : null"
+                     label="Password">
+                <b-input v-model="form.password" password-reveal type="password"></b-input>
+            </b-field>
+
+            <!-- password confirmation -->
+            <b-field label="Confirm password">
+                <b-input v-model="form.password_confirmation" password-reveal type="password"></b-input>
+            </b-field>
+
+            <!-- submit -->
+            <div class="is-flex is-justify-content-flex-end pt-3">
+                <b-button :disabled="form.processing" native-type="submit" type="is-success">Create</b-button>
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "Create"
-    }
+
+export default {
+    data() {
+        return {
+            form: this.$inertia.form({
+                name: null,
+                email: null,
+                password: null,
+                password_confirmation: null,
+            }),
+        }
+    },
+}
 </script>
