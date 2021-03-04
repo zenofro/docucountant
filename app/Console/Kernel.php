9eb0322031a039b8233c85fca9999a1fee5cdf36
Zeno Frooninckx <zeno.frooninckx@hotmail.com>
2021-03-04 22:50:41 +0100

    worker scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Artisan;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,6 +26,10 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        $schedule->call(function (){
+            Artisan::call('queue:work', ['--max-time' => '50']);
+        });
     }
 
     /**
